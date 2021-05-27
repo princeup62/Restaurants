9cf2f0708803777e7acebb0ea535e61b4ee4da0f
princeup62 <princesh208@gmail.com>
2021-05-27 11:10:50 +0530

    get API are added

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
 
 export class RestaurantList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      list: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/restaurant").then((response) =>
+      response.json().then((result) => {
+        this.setState({ list: result });
+      })
+    );
+  }
+
   render() {
-    return <div>RestaurantList</div>;
+    return (
+      <>
+        {this.state.list ? (
+          this.state.list.map((el) => <div>hello</div>)
+        ) : (
+          <p>null</p>
+        )}
+      </>
+    );
   }
 }
 
